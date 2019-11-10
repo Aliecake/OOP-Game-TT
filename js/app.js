3a7b7337579c6ffff17bbe4e98667ef61215bd6a
Lauren Eggers <neyabenz@gmail.com>
2019-11-10 14:32:05 -0500

    rubric fixes : added click

--- js/app.js
+++ js/app.js
@@ -10,23 +10,30 @@ let game;
 let phrases = new Phrase();
 
 document.getElementById('btn__reset').addEventListener('click', () => {
-	game = new Game(phrases.list);
+	game = new Game();
 	game.reset();
-	game.start();
+	game.startGame();
+});
+
+document.addEventListener('click', (e) => {
+	if(e.target.classList.contains('key') && !game.guessedLetters.includes(e.key)){
+		game.guessedLetters.push(e.target.innerText);
+		game.handleInteraction(e.target.innerText);
+	}
 });
 
 document.addEventListener('keyup', (e) => {
 	//checks if letter, game is defined, key is 1 char, and that key has not been previously guessed
 	if(!phrases.regex.test(e.key) && game && e.key.length === 1 && !game.guessedLetters.includes(e.key)){
 		game.guessedLetters.push(e.key);
-		game.interaction(e.key);
+		game.handleInteraction(e.key);
 	}
 });
 
 //called by start
 function displayBoard(game) {
 
-	game.phrase.split('').forEach(letter => {
+	phrase.activePhrase.split('').forEach(letter => {
 		if(letter === ' '){
 			phrases.createElement(`li`, `space`, `phrase`, ` `, 1);
 		} else if (phrases.regex.test(letter)){

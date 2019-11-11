fcf0caa185c3aee0d893cc21c66471d5a2594858
Lauren Eggers <neyabenz@gmail.com>
2019-11-10 20:05:42 -0500

    display moved to phrase class

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
 	* event listeners for the start button and onscreen keyboard buttons.
 	*/
 let game;
-let phrases = new Phrase();
+let phrase = new Phrase();
 
 document.getElementById('btn__reset').addEventListener('click', () => {
 	game = new Game();
@@ -16,7 +16,7 @@ document.getElementById('btn__reset').addEventListener('click', () => {
 });
 
 document.addEventListener('click', (e) => {
-	if(e.target.classList.contains('key') && !game.guessedLetters.includes(e.key)){
+	if(e.target.classList.contains('key') && !game.guessedLetters.includes(e.target.innerText) && game){
 		game.guessedLetters.push(e.target.innerText);
 		game.handleInteraction(e.target.innerText);
 	}
@@ -24,26 +24,11 @@ document.addEventListener('click', (e) => {
 
 document.addEventListener('keyup', (e) => {
 	//checks if letter, game is defined, key is 1 char, and that key has not been previously guessed
-	if(!phrases.regex.test(e.key) && game && e.key.length === 1 && !game.guessedLetters.includes(e.key)){
+	if(!phrase.regex.test(e.key) && game && e.key.length === 1 && !game.guessedLetters.includes(e.key)){
 		game.guessedLetters.push(e.key);
 		game.handleInteraction(e.key);
 	}
 });
-
-//called by start
-function displayBoard(game) {
-
-	phrase.activePhrase.split('').forEach(letter => {
-		if(letter === ' '){
-			phrases.createElement(`li`, `space`, `phrase`, ` `, 1);
-		} else if (phrases.regex.test(letter)){
-			phrases.createElement(`li`, `none`, `phrase`, letter, 1);
-		} else {
-			phrases.createElement(`li`, `letter`, `phrase`, letter, 1, letter);
-		}
-
-	});
-}
 
 function overlayToggle() {
 	document.getElementById('overlay').classList.toggle('is-not-visible');

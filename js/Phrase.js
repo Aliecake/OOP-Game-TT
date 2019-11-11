0ab606bcb32883f98c91fb86b98b2064dff360ad
Lauren Eggers <neyabenz@gmail.com>
2019-11-10 20:06:22 -0500

    showmatched letter

--- js/Phrase.js
+++ js/Phrase.js
@@ -8,13 +8,21 @@
  class Phrase {
     //create phrases
     constructor() {
-       
         this.activePhrase = null;
         this.regex = /[^A-Za-z]/;
 
     }
-    addPhraseToDisplay() {
 
+    addPhraseToDisplay() {
+        this.activePhrase.split('').forEach(letter => {
+            if(letter === ' '){
+                this.createElement(`li`, `space`, `phrase`, ` `, 1);
+            } else if (this.regex.test(letter)){
+                this.createElement(`li`, `special`, `phrase`, letter, 1);
+            } else {
+                this.createElement(`li`, `letter`, `phrase`, letter, 1, letter);
+            }
+        });
     }
    //create an element on page, add class, append - multi use
    //**NOTE: Below default is its alt+255. Spaces are not valid tokens*/
@@ -25,9 +33,12 @@
         this.node.appendChild(this.textnode);
 	    document.getElementById(`${parent}`).childNodes[nodenum].appendChild(this.node);
     }
-    letterHighlight(letter) {
-        document.querySelectorAll(`.${letter}`).forEach(el => {
-            el.classList.add(`show`);
+    //highlight/show phrase guesses, highlight right/wrong keyboard
+    showMatchedLetter(letter, selector, _class) {
+        document.querySelectorAll(`.${selector}`).forEach(el => {
+            if(el.innerText === letter){
+                el.classList.add(_class);
+            }
         });
     }
 
